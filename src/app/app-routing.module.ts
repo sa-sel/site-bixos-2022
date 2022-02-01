fc82f514b4ed6633511fe86c8952c80d462602ed
angelobguido <angelobguido@gmail.com>
2022-02-01 18:40:33 -0300

    Fix #20; Export banner-router from index

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { HomeComponent } from '@core'
 import { TitleComponent } from '@shared'
-import { BannerRouterComponent } from 'core/banner-router/banner-router.component'
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
