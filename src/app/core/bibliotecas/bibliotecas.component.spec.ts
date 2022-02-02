2c0ae42a555a7e0228ab9f2efe21b1c909f10df9
murilo-toddy <murilo.todao@usp.br>
2022-02-02 17:02:51 -0300

    Fix formatting

--- src/app/core/bibliotecas/bibliotecas.component.spec.ts
+++ src/app/core/bibliotecas/bibliotecas.component.spec.ts
@@ -1,25 +1,24 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { BibliotecasComponent } from './bibliotecas.component';
+import { BibliotecasComponent } from './bibliotecas.component'
 
 describe('BibliotecasComponent', () => {
-  let component: BibliotecasComponent;
-  let fixture: ComponentFixture<BibliotecasComponent>;
+  let component: BibliotecasComponent
+  let fixture: ComponentFixture<BibliotecasComponent>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BibliotecasComponent ]
-    })
-    .compileComponents();
-  });
+      declarations: [BibliotecasComponent],
+    }).compileComponents()
+  })
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(BibliotecasComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(BibliotecasComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
