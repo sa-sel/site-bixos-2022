b12bbba324c3f7d8c988ead6fba260dabf3cdb72
murilo-toddy <murilo.todao@usp.br>
2022-02-03 13:22:29 -0300

    Update h2 prop to subtitle in sobre-curso component

--- src/app/core/sobre-curso/sobre-curso.component.ts
+++ src/app/core/sobre-curso/sobre-curso.component.ts
@@ -11,7 +11,8 @@ export class SobreCursoComponent {
       text: 'Sim! Nós também vamos falar um pouco sobre a graduação. Afinal de contas é uma parte importante da universidade.',
     },
     {
-      text: '<h2>"Separação" por Ênfases</h2>',
+      text: '"Separação" por Ênfases',
+      subtitle: true,
     },
     {
       image: {
@@ -22,7 +23,8 @@ export class SobreCursoComponent {
       text: 'Aqui na USP São Carlos, o curso de Engenharia Elétrica é dividido em duas ênfases: <strong>Sistemas de Energia e Automação</strong> e o de <strong>Eletrônica</strong>. Sendo que cada uma dessas ênfases recebe 50 novos alunos por ano, fazendo com que a Elétrica seja o curso com mais bixos na EESC. <br><br>Vale ressaltar que, apesar dessa separação por ênfase, todos são do mesmo curso, sendo coordenados pela SEL (Departamento de Engenharia Elétrica) e representados pela SA-SEL. Em outras palavras, <strong>estamos todos no mesmo barco</strong>.',
     },
     {
-      text: '<h2>Grade Curricular</h2>',
+      text: 'Grade Curricular',
+      subtitle: true,
     },
     {
       image: {
@@ -33,7 +35,8 @@ export class SobreCursoComponent {
       text: 'Todos os cursos de Engenharia da EESC passam por um ciclo de <strong>matérias básicas</strong> (básica é diferente de fácil) nos primeiros anos. Nele são apresentados os conceitos elementares de Matemática, Física, Química entre outras matérias para criar a base teórica do que está por vir. Essas matérias são os famosos Cálculo I, II, III, IV, Física I, II, Química Geral, Isostática, Desenho Técnico, Geometria Analítica e Introdução à Programação entre outras. Também tem algumas matérias de introdução teórica ao curso, como Introdução á Engenharia Elétrica e Projetos em Engenharia Elétrica.<br><br>Haverão também algumas matérias básicas de Elétrica, logo no segundo semestre já tem Medidas e Circuitos Elétricos e no segundo ano começa Circuitos Elétricos I e II, Sistemas Digitais, Laboratório de Medidas, Laboratório de Semicondutores, entre algumas outras que os alunos de ambas as ênfases irão fazer.<br><br>No terceiro ano a divisão das ênfases começa a ficar mais clara com a grade curricular dividindo  e focando mais nas matérias específicas. Vocês podem consultar ambas grades curriculares nos links abaixo:<ul><li><a target="_blank" rel="noopener noreferrer" href="https://uspdigital.usp.br/jupiterweb/listarGradeCurricular?codcg=18&codcur=18045&codhab=0&tipo=N">Eletrônica</a></li><li><a target="_blank" rel="noopener noreferrer" href="https://uspdigital.usp.br/jupiterweb/listarGradeCurricular?codcg=18&codcur=18050&codhab=0&tipo=N">Sistemas de Energia e Automação</a></li></ul>',
     },
     {
-      text: '<h2>Proposta Do Curso, Projeto Pedagógico e Certificados Especiais</h2>',
+      text: 'Proposta Do Curso, Projeto Pedagógico e Certificados Especiais',
+      subtitle: true,
     },
     {
       image: {
