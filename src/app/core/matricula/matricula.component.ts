089231cc80deb9a65ecbe48ab618966b981f47e7
Daniel Shinji <d.takahashi.s@outlook.com>
2022-02-03 18:27:53 -0300

    Fix requested changes

--- src/app/core/matricula/matricula.component.ts
+++ src/app/core/matricula/matricula.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'app-matricula',
   templateUrl: './matricula.component.html',
-  styleUrls: ['./matricula.component.scss'],
 })
 export class MatriculaComponent {}
