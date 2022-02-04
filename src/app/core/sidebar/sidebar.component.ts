822e6ce435cc244765be5f498555a1ca587a5bca
Fernando Cirilo Zanchetta <fe.cz@usp.br>
2022-02-04 14:15:02 -0300

    Add sidebar styles and functions

--- src/app/core/sidebar/sidebar.component.ts
+++ src/app/core/sidebar/sidebar.component.ts
@@ -1,4 +1,13 @@
-import { Component, Input } from '@angular/core'
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core'
 import { SidebarModel } from '@models'
 
 @Component({
@@ -6,13 +15,25 @@ import { SidebarModel } from '@models'
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnChanges {
+  @ViewChild('sidebar') sidebar!: TemplateRef<any>
 
   @Input() sidebarItems!: SidebarModel
-  @Input() isCollapsed!: boolean
+  @Input() public isSidebarCollapsed = true
 
-  constructor() {
-    this.isCollapsed = !this.isCollapsed
+  toggleCollapse() {
+    this.isSidebarCollapsed = !this.isSidebarCollapsed
+  }
+
+  // for two-way data binding
+  @Output() isCollapsedChange = new EventEmitter<boolean>()
+
+  ngOnChanges(changes: SimpleChanges): void {
+  // eslint-disable-next-line
+    if (changes['isCollapsed'].currentValue) {
+      this.isSidebarCollapsed = false
+      this.isCollapsedChange.emit(this.isSidebarCollapsed)
+    }
   }
 
 }
