205bf2f89b015ff43c05ef0a0185f450c5ab969a
Fernando Cirilo Zanchetta <fe.cz@usp.br>
2022-02-04 23:33:21 -0300

    Change the way the text and the embed video are shown

--- src/app/core/campus/campus.component.ts
+++ src/app/core/campus/campus.component.ts
@@ -7,12 +7,6 @@ import { AlternatingLayoutModel } from 'models/alternating-layout.model'
 })
 export class CampusComponent {
   textAndImageList: AlternatingLayoutModel[] = [
-    {
-      text: 'Todos os veteranos já foram bixos e se perderam pelo menos uma vez na universidade; falar que um bixo é perdido é como falar que a água é molhada. Porém o importante é uma hora acertar o caminho, por isso que estamos disponibilizando algumas dicas para ajudá-lo(a) a se localizar na USP - São Carlos.',
-    },
-    {
-      text: 'Primeiramente é importante saber que existem dois campus da USP em São Carlos, o campus 1 no centro da cidade, este será o local que você realizará a matrícula, passará pela semana de recepção e terá a maioria das suas aulas.',
-    },
     /* {
       image: {
         alt: 'Prédio E1',
@@ -41,15 +35,5 @@ export class CampusComponent {
         caption: 'Uma sala de aula no bloco D, no campus 1',
       },
     }, */
-    {
-      text: 'E o campus 2 que se encontra em uma região mais afastada da cidade e concentra a graduação de alguns cursos como Eng. de Computação, Ambiental e Materiais, no entanto você provavelmente terá as aulas de laboratório de física e química lá, portanto também é importante conhecê-lo para evitar ficar perdido.<br>',
-    },
-    {
-      text: 'Quer ver mais fotos do CAASO?<br> Confira no álbum <a target="_blank" rel="noopener noreferrer" href="https://photos.app.goo.gl/3msA5jNUZwsLsUa58">"USP - São Carlos"</a>!',
-    },
-    {
-      text: '<iframe width="1120" height="630" src="https://www.youtube-nocookie.com/embed/GCKL3OJyfHI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-    },
   ]
-
 }
