a8df329b1171042af9c1700ccf2a117242b3cbe0
Daniel Shinji <d.takahashi.s@outlook.com>
2022-02-05 15:55:51 -0300

    Fix requested changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -39,7 +39,7 @@ const routes: Routes = [
     },
   },
   {
-    path: 'servicosacademicos',
+    path: 'servicos-academicos',
     component: ServicosAcademicosComponent,
     data: {
       background: [
