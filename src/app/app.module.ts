ddebb168f270eee593edb033e818205fac5053ce
Fernando Cirilo Zanchetta <fe.cz@usp.br>
2022-02-04 21:42:36 -0300

    Create campus component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { CarouselModule } from 'ngx-bootstrap/carousel'
 import { BannerRouterComponent } from './core/banner-router/banner-router.component'
 import { MatriculaComponent } from './core/matricula/matricula.component'
 import { BandejaoComponent } from './core/bandejao/bandejao.component'
+import { CampusComponent } from './core/campus/campus.component'
 
 @NgModule({
   declarations: [
@@ -38,6 +39,7 @@ import { BandejaoComponent } from './core/bandejao/bandejao.component'
     BannerRouterComponent,
     MatriculaComponent,
     BandejaoComponent,
+    CampusComponent,
   ],
   imports: [
     AppRoutingModule,
