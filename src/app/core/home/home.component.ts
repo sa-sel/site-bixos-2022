4cfa25785817832220b30a4468a6b94bb1da9125
lima-douglas <douglas.lima@usp.br>
2022-02-07 12:36:08 -0300

    Write text for home screen

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core'
+import { Component, OnInit, Input } from '@angular/core'
+import { ImageGridItemModel,  SocialMedia, SocialMediaModel  } from '@models'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  sponsorsPhotos: ImageGridItemModel[] = [
+    {
+      image: {
+        src: '/assets/images/home/abias.svg',
+        alt: 'abias',
+        caption: 'Abias Imóveis',
+      },
+    },
+    {
+      image: {
+        src: '/assets/images/home/candma.svg',
+        alt: 'candma',
+        caption: 'Candma',
+      },
+    },
+    {
+      image: {
+        src: '/assets/images/home/cicbeu.svg',
+        alt: 'cicbeu',
+        caption: 'CICBEU',
+      },
+    },
+  ]
+  @Input() size = 23
+  @Input() color = ''
+  ngOnInit(): void {
+    this.color = this.color ? `text-${this.color}` : ''
+  }
+
+  groups: SocialMediaModel[] = [
+    { name: SocialMedia.Facebook, url: 'https://www.facebook.com/sasel.usp' },
+    { name: SocialMedia.WhatsApp, url: '+55 (71) 8155-6248' },
+    { name: SocialMedia.Discord, url: 'https://github.com/sa-sel' },
+  ]
+}
+/*
   textAndImageList = [
     {
       image: {
@@ -87,4 +124,4 @@ export class HomeComponent {
   toggleModal() {
     this.openModal = !this.openModal
   }
-}
+  */
