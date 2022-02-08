f155e4bdd88b68d19b552300a3e845870289ff23
lima-douglas <douglas.lima@usp.br>
2022-02-07 22:00:08 -0300

    Add navbar icon

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core'
-import { ImageGridItemModel,  SocialMedia, SocialMediaModel  } from '@models'
+import { Component, Input } from '@angular/core'
+import { ImageGridItemModel, SocialMedia, SocialMediaModel } from '@models'
+import { SidebarService } from '@services'
 
 @Component({
   selector: 'app-home',
@@ -13,6 +14,7 @@ export class HomeComponent {
         alt: 'abias',
         caption: 'Abias Imóveis',
       },
+      url: 'https://www.abiasimoveis.com.br/',
     },
     {
       image: {
@@ -20,6 +22,7 @@ export class HomeComponent {
         alt: 'candma',
         caption: 'Candma',
       },
+      url: 'http://caandma.com.br/',
     },
     {
       image: {
@@ -27,6 +30,7 @@ export class HomeComponent {
         alt: 'cicbeu',
         caption: 'CICBEU',
       },
+      url: 'http://www.cicbeu.com.br/',
     },
   ]
   @Input() size = 23
@@ -40,88 +44,6 @@ export class HomeComponent {
     { name: SocialMedia.WhatsApp, url: '+55 (71) 8155-6248' },
     { name: SocialMedia.Discord, url: 'https://github.com/sa-sel' },
   ]
+
+  constructor(public sidebarService: SidebarService) {}
 }
-/*
-  textAndImageList = [
-    {
-      image: {
-        alt: 'banner',
-        src: '../assets/images/banner.jpg',
-        caption: 'Teste',
-      },
-      text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
-    },
-    {
-      text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
-    },
-    {
-      image: {
-        alt: 'banner',
-        src: '../assets/images/banner.jpg',
-        caption: 'Teste',
-      },
-      text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
-    },
-    {
-      image: {
-        alt: 'banner',
-        src: '../assets/images/banner.jpg',
-        caption: 'Teste',
-      },
-    },
-    {
-      image: {
-        alt: 'banner',
-        src: '../assets/images/banner.jpg',
-      },
-    },
-    {
-      image: {
-        alt: 'banner',
-        src: '../assets/images/banner.jpg',
-        caption: 'Teste',
-      },
-      text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem IpsumLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem IpsumLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem IpsumLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem IpsumLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum versions of Lorem Ipsum',
-    },
-  ]
-
-  openModal = false
-
-  image1 = {
-    image: {
-      src: '../../../assets/images/banner.jpg',
-      alt: 'uma foto da comissao',
-      caption: 'uma foto da comissao',
-    },
-    modal: {
-      image: {
-        src: '../../../assets/images/banner.jpg',
-        alt: 'uma foto da comissao',
-        caption: 'testinho',
-      },
-      text: 'oioioitesteeee',
-      title: 'OIIIII',
-    },
-  }
-
-  image2 = {
-    image: {
-      src: 'https://freepikpsd.com/file/2019/10/generic-company-logo-png-7-Transparent-Images.png',
-      alt: 'imagem teste',
-      caption: 'imagem teste',
-    },
-    url: 'https://google.com',
-  }
-
-  image3 = {
-    image: {
-      src: 'https://freepikpsd.com/file/2019/10/generic-company-logo-png-7-Transparent-Images.png',
-      alt: 'uma logo qualquer',
-    },
-    url: 'https://google.com',
-  }
-
-  toggleModal() {
-    this.openModal = !this.openModal
-  }
-  */
