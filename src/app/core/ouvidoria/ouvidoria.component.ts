a29e81c4ddc6ce2a0704590a8b811ec62c682068
Lucas Viana <oviana@outlook.com>
2022-02-08 23:55:16 -0300

    #160 - Fix embedded forms ratio

--- src/app/core/ouvidoria/ouvidoria.component.ts
+++ src/app/core/ouvidoria/ouvidoria.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'app-ouvidoria',
   templateUrl: './ouvidoria.component.html',
+  styleUrls: ['./ouvidoria.component.scss'],
 })
 export class OuvidoriaComponent {}
